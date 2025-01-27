778e887d020b3ca3932b5369b7416b76e82348b7
owenwillette <owenwillette94@gmail.com>
2025-01-26 23:21:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+print('Hello World')
+Hello World
+name = input('Owen')
+Owen
+>>> age = input('18')
+18
+>>> height = input('6')
+is_student = input(True)
+True
+displayValues(name,age,height,is_student)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'displayValues' is not defined
+year1 = int('2025')
+>>> year2 = int('2028')
+>>> difference = (year2 - year1)
+>>> print(difference)
+3
+fahrenheight = float('42.8')
+>>> celsius = (fahrenheight -32) * 5/9
+>>> print(fahrenheight)
+42.8
+>>> print(celsius)
+5.999999999999998
+>>> 
+exchange_rate = 0.97
+>>> usd = int('1')
+>>> eu = usd * exchange_rate
+>>> print(usd)
+1
+>>> print(eu)
+0.97
+>>> 
+
+
